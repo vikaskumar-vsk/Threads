2828064591bb9459ea8bd4582976cfe711ed29ac
vikaskumar-vsk <vikaskumar.vsk@gmail.com>
2017-02-16 16:35:56 +0530

    Using handler in threaded app

--- app/src/main/java/com/vikaskumar/vsk/threads/MainActivity.java
+++ app/src/main/java/com/vikaskumar/vsk/threads/MainActivity.java
@@ -4,8 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.os.Handler;
+import android.os.Message;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Used for update user Interface
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            TextView buckysText = (TextView) findViewById(R.id.buckysText);
+            buckysText.setText("Good job Hoss!");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +25,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickBuckysButton(View view) {
-        long futureTime = System.currentTimeMillis() + 10000;
-        while (System.currentTimeMillis() < futureTime) {
-            synchronized (this) {
-                try {
-                    wait(futureTime - System.currentTimeMillis());
-                } catch (Exception e) {
+
+        //Used for codes that run for long time and makes it run in the background.
+        // It is not used for UI update, its bad practice.
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                long futureTime = System.currentTimeMillis() + 10000;
+                while (System.currentTimeMillis() < futureTime) {
+                    synchronized (this) {
+                        try {
+                            wait(futureTime - System.currentTimeMillis());
+                        } catch (Exception e) {
+                        }
+                    }
                 }
+                handler.sendEmptyMessage(0);
             }
-            TextView buckysText = (TextView) findViewById(R.id.buckysText);
-            buckysText.setText("Good job Hoss!");
-        }
+        };
+        Thread buckysThread = new Thread(r);
+        buckysThread.start();
+
     }
 }
